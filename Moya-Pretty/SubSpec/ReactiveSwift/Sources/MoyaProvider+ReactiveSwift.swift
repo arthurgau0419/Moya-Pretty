60921a325676c330a296689db018c12a5bcb70e3
arthurgau0419 <arthurgau0419@gmail.com>
2020-02-25 17:05:59 +0800

    Update Spec to 3.0.0 (Swift5, Moya14, Alamofire5).

--- Moya-Pretty/SubSpec/ReactiveSwift/Sources/MoyaProvider+ReactiveSwift.swift
+++ Moya-Pretty/SubSpec/ReactiveSwift/Sources/MoyaProvider+ReactiveSwift.swift
@@ -1,7 +1,6 @@
 import Foundation
 import Moya
 import ReactiveSwift
-import Result
 #if canImport(Japx)
 import Japx
 #endif
@@ -20,9 +19,9 @@ extension Reactive where Base: MoyaProviderType, Base.Target: DecodableType  {
 
     return request(token, callbackQueue: callbackQueue)
       .attemptMap { response -> Result<Base.Target.DecodableModel, MoyaError> in
-        return Result<Base.Target.DecodableModel, MoyaError>(catching: { () -> Base.Target.DecodableModel in
+        return Result<Base.Target.DecodableModel, Error>(catching: { () -> Base.Target.DecodableModel in
           return try response.toModel(target: token, atKeyPath: keyPath, using: decoder, failsOnEmptyData: failsOnEmptyData)
-        })
+            }).mapError({ $0 as! MoyaError })
     }
   }
 }
@@ -36,11 +35,10 @@ public extension Reactive where Base: MoyaProviderType, Base.Target: JapxDecodab
   ///   - callbackQueue: Callback queue. If nil - queue from provider initializer will be used.
   /// - Returns: SignalProducer<Base.Target.DecodableModel, MoyaError>.
   public func requestModel(_ token: Base.Target, using decoder: JapxDecoder? = nil, includeList: String? = nil, callbackQueue: DispatchQueue? = nil) -> SignalProducer<Base.Target.DecodableModel, MoyaError> {
-    return request(token, callbackQueue: callbackQueue)
-      .attemptMap { response -> Result<Base.Target.DecodableModel, MoyaError> in
-        Result<Base.Target.DecodableModel, MoyaError>(catching: { () -> Base.Target.DecodableModel in
-          try response.toModel(target: token, using: decoder, includeList: includeList)
-        })
+    return request(token, callbackQueue: callbackQueue).attemptMap { response -> Result<Base.Target.DecodableModel, MoyaError> in
+      Result<Base.Target.DecodableModel, Error>(catching: { () -> Base.Target.DecodableModel in
+        try response.toModel(target: token, using: decoder, includeList: includeList)
+      }).mapError { $0 as! MoyaError }
     }
   }
 }
@@ -55,11 +53,10 @@ public extension Reactive where Base: MoyaProviderType, Base.Target: MappableRes
   ///   - callbackQueue: Callback queue. If nil - queue from provider initializer will be used.
   /// - Returns: SignalProducer<Base.Target.MappableResponseModel, MoyaError>.
   public func requestModel(_ token: Base.Target, option: MapperOption? = nil, callbackQueue: DispatchQueue? = nil) -> SignalProducer<Base.Target.MappableResponseModel, MoyaError> {
-    return request(token, callbackQueue: callbackQueue)
-      .attemptMap { response -> Result<Base.Target.MappableResponseModel, MoyaError> in
-        Result<Base.Target.MappableResponseModel, MoyaError>(catching: { () -> Base.Target.MappableResponseModel in
-          try response.toModel(target: token, option: option)
-        })
+    return request(token, callbackQueue: callbackQueue).attemptMap { response -> Result<Base.Target.MappableResponseModel, MoyaError> in
+      Result<Base.Target.MappableResponseModel, Error>(catching: { () -> Base.Target.MappableResponseModel in
+        try response.toModel(target: token, option: option)
+      }).mapError { $0 as! MoyaError }
     }
   }
 }
@@ -74,11 +71,10 @@ public extension Reactive where Base: MoyaProviderType, Base.Target: MappableRes
   ///   - callbackQueue: Callback queue. If nil - queue from provider initializer will be used.
   /// - Returns: SignalProducer<Base.Target.MappableResponseModel, MoyaError>.
   public func requestXmlModel(_ token: Base.Target, option: MapperOption? = nil, callbackQueue: DispatchQueue? = nil) -> SignalProducer<Base.Target.MappableResponseModel, MoyaError> {
-    return request(token, callbackQueue: callbackQueue)
-      .attemptMap { response -> Result<Base.Target.MappableResponseModel, MoyaError> in
-        Result<Base.Target.MappableResponseModel, MoyaError>(catching: { () -> Base.Target.MappableResponseModel in
-          try response.toXmlModel(target: token, option: option)
-        })
+    return request(token, callbackQueue: callbackQueue).attemptMap { response -> Result<Base.Target.MappableResponseModel, MoyaError> in
+      Result<Base.Target.MappableResponseModel, Error>(catching: { () -> Base.Target.MappableResponseModel in
+        try response.toXmlModel(target: token, option: option)
+      }).mapError { $0 as! MoyaError }
     }
   }
 }
